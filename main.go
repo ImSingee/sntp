9ecdcc7bbd7d2fc85bf01135f6c877726b4aa9c1
Singee <git@singee.me>
2020-12-13 02:41:24 +0800

    change main

--- main.go
+++ main.go
@@ -6,11 +6,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/btfak/sntp/netapp"
 	"github.com/btfak/sntp/netevent"
 )
 
 func main() {
+	fmt.Println("Modified Version!")
+
 	var handler = netapp.GetHandler()
 	netevent.Reactor.ListenUdp(123, handler)
 	netevent.Reactor.Run()
